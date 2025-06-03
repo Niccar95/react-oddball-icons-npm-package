e78a2d54503e86ab64e4003bfe8f47dbbae43ac0
Niccar95 <niccar.95@outlook.com>
2025-06-03 14:24:15 +0200

    rebuilt pack with new imports

--- package/index.js
+++ package/index.js
@@ -1,18 +1,18 @@
-export { default as Avatar } from "./components/Avatar";
-export { default as AvatarAlt } from "./components/AvatarAlt";
-export { default as Calculator } from "./components/Calculator";
-export { default as Calender } from "./components/Calender";
-export { default as Clock } from "./components/Clock";
-export { default as Document } from "./components/Document";
-export { default as Envelope } from "./components/Envelope";
-export { default as Lock } from "./components/Lock";
-export { default as MagnifyingGlass } from "./components/MagnifyingGlass";
-export { default as Presentation } from "./components/Presentation";
-export { default as SpeechBubble } from "./components/SpeechBubble";
-export { default as SpeechBubbleAlt } from "./components/SpeechBubbleAlt";
-export { default as StapleDiagram } from "./components/StapleDiagram";
-export { default as StapleDiagramAlt } from "./components/StapleDiagramAlt";
-export { default as Suitcase } from "./components/Suitcase";
+export { default as Avatar } from "./Avatar";
+export { default as AvatarAlt } from "./AvatarAlt";
+export { default as Calculator } from "./Calculator";
+export { default as Calender } from "./Calender";
+export { default as Clock } from "./Clock";
+export { default as Document } from "./Document";
+export { default as Envelope } from "./Envelope";
+export { default as Lock } from "./Lock";
+export { default as MagnifyingGlass } from "./MagnifyingGlass";
+export { default as Presentation } from "./Presentation";
+export { default as SpeechBubble } from "./SpeechBubble";
+export { default as SpeechBubbleAlt } from "./SpeechBubbleAlt";
+export { default as StapleDiagram } from "./StapleDiagram";
+export { default as StapleDiagramAlt } from "./StapleDiagramAlt";
+export { default as Suitcase } from "./Suitcase";
 
 export function isOddBall(string) {
   return string === "oddball";
