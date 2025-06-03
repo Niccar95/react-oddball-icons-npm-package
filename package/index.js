f7fd32631ca49e86cab16e945cb46c664a940ed8
Niccar95 <niccar.95@outlook.com>
2025-06-03 12:33:24 +0200

    changed imports to ES

--- package/index.js
+++ package/index.js
@@ -1,38 +1,19 @@
-const Avatar = require("./components/Avatar");
-const AvatarAlt = require("./components/AvatarAlt");
-const Calculator = require("./components/Calculator");
-const Calender = require("./components/Calender");
-const Clock = require("./components/Clock");
-const Document = require("./components/Document");
-const Envelope = require("./components/Envelope");
-const Lock = require("./components/Lock");
-const MagnifyingGlass = require("./components/MagnifyingGlass");
-const Presentation = require("./components/Presentation");
-const SpeechBubble = require("./components/SpeechBubble");
-const SpeechBubbleAlt = require("./components/SpeechBubbleAlt");
-const StapleDiagram = require("./components/StapleDiagram");
-const StapleDiagramAlt = require("./components/StapleDiagramAlt");
-const Suitcase = require("./components/Suitcase");
+export { default as Avatar } from "./components/Avatar";
+export { default as AvatarAlt } from "./components/AvatarAlt";
+export { default as Calculator } from "./components/Calculator";
+export { default as Calender } from "./components/Calender";
+export { default as Clock } from "./components/Clock";
+export { default as Document } from "./components/Document";
+export { default as Envelope } from "./components/Envelope";
+export { default as Lock } from "./components/Lock";
+export { default as MagnifyingGlass } from "./components/MagnifyingGlass";
+export { default as Presentation } from "./components/Presentation";
+export { default as SpeechBubble } from "./components/SpeechBubble";
+export { default as SpeechBubbleAlt } from "./components/SpeechBubbleAlt";
+export { default as StapleDiagram } from "./components/StapleDiagram";
+export { default as StapleDiagramAlt } from "./components/StapleDiagramAlt";
+export { default as Suitcase } from "./components/Suitcase";
 
-function isOddBall(string) {
+export function isOddBall(string) {
   return string === "oddball";
 }
-
-module.exports = {
-  isOddBall,
-  Avatar,
-  AvatarAlt,
-  Calculator,
-  Calender,
-  Clock,
-  Document,
-  Envelope,
-  Lock,
-  MagnifyingGlass,
-  Presentation,
-  SpeechBubble,
-  SpeechBubbleAlt,
-  StapleDiagram,
-  StapleDiagramAlt,
-  Suitcase,
-};
